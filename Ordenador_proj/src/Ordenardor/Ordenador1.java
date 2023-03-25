b1bc51fdfac6c6e385803a8268cacc43e03a6b0b
dvmanieri <128916125+dvmanieri@users.noreply.github.com>
2023-03-25 12:09:53 -0300

    Adicionei arquivos via upload

---
+++ Ordenador_proj/src/Ordenardor/Ordenador1.java
@@ -0,0 +1,10 @@
+package Ordenardor;
+
+public class Ordenador1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Ordenador_proj/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Dvmanieri
+ *
+ */
+module Ordenador_proj {
+}
